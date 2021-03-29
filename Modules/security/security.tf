5209b457cde72dd3557324dfa54c603f0dd891bc
prithviraj-globant <81555030+prithviraj-globant@users.noreply.github.com>
2021-03-29 13:41:47 +0530

    Add files via upload

---
+++ Modules/security/security.tf
@@ -0,0 +1,70 @@
+variable "vpc-id" {}
+resource "aws_security_group" "myinstance" {
+  vpc_id      = "${var.vpc-id}"
+  name        = "test_sg_1"
+  description = "security group for my instance"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.elb-securitygroup.id]
+  }
+
+  tags = {
+    Name = "myinstance"
+  }
+}
+
+resource "aws_security_group" "elb-securitygroup" {
+  vpc_id      = "${var.vpc-id}"
+  name        = "elb"
+  description = "security group for load balancer"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "elb"
+  }
+}
+output "instance-sg" {
+  value = aws_security_group.myinstance.id
+}
+output "loadbalancer-sg" {
+  value = aws_security_group.elb-securitygroup.id
+}
